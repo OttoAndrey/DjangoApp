0ce5e7a25e2304d87cd5bef931ca6cd6ae8bdd8a
Ray <ottotruepro@gmail.com>
2019-06-21 18:12:03 +0700

    templates

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def posts_list(request):
-    return HttpResponse('<h1>Kek</h1>')
+    return render(request, 'blog/index.html')
