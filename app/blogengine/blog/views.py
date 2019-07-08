a45e65ab10ae22d5221b6e152dbc9ad0af86e977
Ray <ottotruepro@gmail.com>
2019-07-08 21:58:07 +0700

    Редактирование постов и тегов

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import Post, Tag
-from .utils import ObjectDetailMixin, ObjectCreateMixin
+from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectUpdateMixin
 from .forms import TagForm, PostForm
 
 
@@ -22,9 +22,14 @@ class PostDetail(ObjectDetailMixin, View):
 
 
 class PostCreate(ObjectCreateMixin, View):
-
     form_model = PostForm
     template = 'blog/post_create_form.html'
+
+
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
 
 
 class TagDetail(ObjectDetailMixin, View):
@@ -32,7 +37,12 @@ class TagDetail(ObjectDetailMixin, View):
     template = 'blog/tag_detail.html'
 
 
-class TagCreate(ObjectCreateMixin, View):
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
 
+
+class TagCreate(ObjectCreateMixin, View):
     form_model = TagForm
     template = 'blog/tag_create.html'
