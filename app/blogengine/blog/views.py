0ec3a113a762ff726aab9fe682cfb0bf6db34224
Ray <ottotruepro@gmail.com>
2019-07-11 16:07:32 +0700

    Ограничение прав для простого пользователя

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Tag
 from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectUpdateMixin, ObjectDeleteMixin
 from .forms import TagForm, PostForm
@@ -22,21 +23,24 @@ class PostDetail(ObjectDetailMixin, View):
     template = 'blog/post_detail.html'
 
 
-class PostDelete(ObjectDeleteMixin, View):
-    model = Post
-    template = 'blog/post_delete_form.html'
-    redirect_url = 'posts_list_url'
-
-
-class PostCreate(ObjectCreateMixin, View):
+class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     form_model = PostForm
     template = 'blog/post_create_form.html'
+    raise_exception = True
 
 
-class PostUpdate(ObjectUpdateMixin, View):
+class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
     model = Post
     model_form = PostForm
     template = 'blog/post_update_form.html'
+    raise_exception = True
+
+
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
+    raise_exception = True
 
 
 class TagDetail(ObjectDetailMixin, View):
@@ -44,18 +48,22 @@ class TagDetail(ObjectDetailMixin, View):
     template = 'blog/tag_detail.html'
 
 
-class TagUpdate(ObjectUpdateMixin, View):
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
+    form_model = TagForm
+    template = 'blog/tag_create.html'
+    raise_exception = True
+
+
+class TagUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
     model = Tag
     model_form = TagForm
     template = 'blog/tag_update_form.html'
+    raise_exception = True
 
 
-class TagDelete(ObjectDeleteMixin, View):
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     model = Tag
     template = 'blog/tag_delete_form.html'
     redirect_url = 'tags_list_url'
+    raise_exception = True
 
-
-class TagCreate(ObjectCreateMixin, View):
-    form_model = TagForm
-    template = 'blog/tag_create.html'
